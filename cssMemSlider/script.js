f030f8921b76fd5a7fc3880f9dfd01eebe070b93
Nekoguard <nekoguard@protonmail.com>
2021-10-16 20:19:16 +0300

    feat: add slider design 2021-10-16 20:19:16

---
+++ cssMemSlider/script.js
@@ -0,0 +1,7 @@
+const slidesWrapper = document.querySelector('.mem-slider__slides');
+const slidesInner = document.querySelector('.slides__inner');
+const slides = document.querySelectorAll('.slide');
+const textsWrapper = document.querySelector('.mem-slider__texts');
+const textsInner = document.querySelector('.texts__inner');
+const texts = document.querySelectorAll('.text');
+const dots = document.querySelectorAll('.mem-slider__dot');
